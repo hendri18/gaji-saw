b62dac923a051bb4ed95ae732329cda78c3d789b
Hendri Heryanto <hendri.heryanto8@gmail.com>
2024-03-03 23:45:10 +0700

    separate staff view and atasan view

--- resources/js/pages/PengambilanKeputusan.vue
+++ resources/js/pages/PengambilanKeputusan.vue
@@ -1,80 +1,106 @@
 <template>
-    <h5 v-if="show_history">Histori Pengambilan Keputusan Tanggal: {{ tgl_histori }}</h5>
-    <div class="card">
-        <div class="card-header">Data Karyawan</div>
-        <div class="card-body">
-            <table class="display table table-bordered">
-                <thead>
-                    <tr>
-                        <th v-for="column in columns">{{ column.title }}</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="(kk, i) in karyawan_kriteria">
-                        <td v-for="data in kk">{{ data.key }}</td>
-                    </tr>
-                </tbody>
-            </table>
+    <template v-if="type === 'result'">
+        <div class="d-flex align-items-center mb-3">
+            <button type="button" class="btn btn-secondary me-3" @click="resetHistory" v-if="hasRoles('staff')">Kembali</button>
+            <h5 v-if="show_history">Histori Pengambilan Keputusan Tanggal: {{ tgl_histori }}</h5>
         </div>
-    </div>
-
-    <div class="card mt-3">
-        <div class="card-header">Normalisasi</div>
-        <div class="card-body">
-            <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        <th v-for="column in columns">{{ column.title }}</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="(kk, i) in normalization">
-                        <td v-for="data in kk">{{ data.value }}</td>
-                    </tr>
-                </tbody>
-            </table>
+        <div class="card">
+            <div class="card-header">Data Karyawan</div>
+            <div class="card-body">
+                <table class="display table table-bordered">
+                    <thead>
+                        <tr>
+                            <th v-for="column in columns">{{ column.title }}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(kk, i) in karyawan_kriteria">
+                            <td v-for="data in kk">{{ data.key }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
-    </div>
-
-    <div class="card my-3">
-        <div class="card-header">Hasil</div>
-        <div class="card-body">
-            <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        <th>Ranking</th>
-                        <th>Nama</th>
-                        <th>Nilai Akhir</th>
-                        <th>Persentase</th>
-                        <th>Kenaikan Gaji</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="(ranking, i) in rankings">
-                        <td>{{ ranking.rank }}</td>
-                        <td>{{ ranking.nama }}</td>
-                        <td>{{ ranking.total }}</td>
-                        <td>{{ ranking.percentage }}</td>
-                        <td><b>{{ ranking.salary_increase }}</b></td>
-                    </tr>
-                </tbody>
-            </table>
+    
+        <div class="card mt-3">
+            <div class="card-header">Normalisasi</div>
+            <div class="card-body">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th v-for="column in columns">{{ column.title }}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(kk, i) in normalization">
+                            <td v-for="data in kk">{{ data.value }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
-    </div>
-    <div class="mb-3 d-flex justify-content-between">
-        <div>
-            <div class="d-flex align-items-center">
-                Histori: <div class="ml-2 d-flex"><select name="tgl_histori" v-model="tgl_histori" id=""><option :value="date" v-for="date in dates">{{ date }}</option></select><button class="btn btn-primary" :class="tgl_histori === '' ? 'disabled' : ''" @click="getHistory">Tampilkan</button><button class="btn btn-danger ml-2" v-if="show_history" @click="resetHistory">Reset</button></div>
+    
+        <div class="card my-3">
+            <div class="card-header">Hasil</div>
+            <div class="card-body">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Ranking</th>
+                            <th>Nama</th>
+                            <th>Nilai Akhir</th>
+                            <th>Persentase</th>
+                            <th>Kenaikan Gaji</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(ranking, i) in rankings">
+                            <td>{{ ranking.rank }}</td>
+                            <td>{{ ranking.nama }}</td>
+                            <td>{{ ranking.total }}</td>
+                            <td>{{ ranking.percentage }}</td>
+                            <td><b>{{ ranking.salary_increase }}</b></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="mb-3 d-flex justify-content-between" v-if="hasRoles('atasan')">
+            <div>
+                <div class="d-flex align-items-center">
+                    Histori: <div class="ml-2 d-flex"><select name="tgl_histori" v-model="tgl_histori" id=""><option :value="date" v-for="date in dates">{{ date }}</option></select><button class="btn btn-primary" :class="tgl_histori === '' ? 'disabled' : ''" @click="getHistory">Tampilkan</button><button class="btn btn-danger ms-2" v-if="show_history" @click="resetHistory">Reset</button></div>
+                </div>
+                
+            </div>
+            <div>
+                <a href="/export" class="btn btn-success mr-2">Export Excel</a>
+                <a href="/export?type=pdf" class="btn btn-secondary mr-2">Export PDF</a>
+                <button type="button" class="btn btn-primary" @click="save" v-if="hasRoles('atasan')">Simpan</button>
             </div>
             
         </div>
-        <div>
-            <a href="/export" class="btn btn-success mr-2">Export Excel</a>
-            <a href="/export?type=pdf" class="btn btn-secondary mr-2">Export PDF</a>
-            <button type="button" class="btn btn-primary" @click="save">Simpan</button>
+    </template>
+    <template v-if="type === 'table'">
+        <div class="card">
+            <div class="card-header">Laporan Kenaikan Gaji</div>
+            <div class="card-body">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Tanggal</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="date in dates">
+                            <td>{{ date }}</td>
+                            <td><button type="button" class="btn btn-primary" @click="getHistoryByDate(date)">Lihat</button></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
-        
-    </div>
+    </template>
 </template>
 <script>
 
@@ -90,9 +116,11 @@ export default {
         const tgl_histori = ref('');
         const show_history = ref(false);
         const dates = ref([]);
+        const type = ref('result')
 
         onMounted (async () => {
             try {
+                type.value = hasRoles('atasan') ? 'result' : 'table';
                 const resp = await axios.get(`api/all-data`);
                 columns.value = resp.data.data.columns;
                 karyawan_kriteria.value = resp.data.data.karyawan_kriteria;
@@ -150,6 +178,12 @@ export default {
             }
         }
 
+        const getHistoryByDate = async (date) => {
+            tgl_histori.value = date;
+            type.value = 'result';
+            getHistory();
+        }
+
         const resetHistory = async () => {
             try {
                 const resp = await axios.get(`api/all-data`);
@@ -159,6 +193,7 @@ export default {
                 rankings.value = resp.data.data.rankings;
                 show_history.value = false;
                 tgl_histori.value = '';
+                type.value = hasRoles('atasan') ? 'result' : 'table';
                 window.scrollTo({ top: 0, behavior: 'smooth' });
 
             } catch (error) {
@@ -168,8 +203,12 @@ export default {
             }
         }
 
+        const hasRoles = (roles) => {
+            return window.hasRoles(roles)
+        }
+        
         return {
-            columns, crips, karyawan_kriteria, normalization, rankings, save, tgl_histori, show_history, getHistory, resetHistory, dates
+            columns, crips, karyawan_kriteria, normalization, rankings, save, tgl_histori, show_history, getHistory, getHistoryByDate, resetHistory, dates, hasRoles, type
         }
     },
     mounted() {
